92b9ca72a10e572acdf53c44a283941ed5f98997
Adrien M <adrien@d.tube>
2018-01-30 01:50:24 +0100

    add graph to stats

--- statistics.js
+++ statistics.js
@@ -2,7 +2,6 @@
 
 function getCurrentStats() {
     return {
-        buffered: player.bufferedEnd() - player.currentTime(),
         droppedVideoFrames: player.getVideoPlaybackQuality().droppedVideoFrames,
         totalVideoFrames: player.getVideoPlaybackQuality().totalVideoFrames,
         paused: player.paused(),
@@ -14,18 +13,26 @@ function getCurrentStats() {
         gateway: player.currentSource().src.split('/')[2],
         fileSrc: player.currentSource().src,
         fileType: player.currentSource().type,
+        buffered: player.bufferedEnd() - player.currentTime(),
     }
 }
 
 function statisticsPlugin(options) {
     var isStarted = false
-
+    var interval = null
+    this.on('stopStats', function() {
+        clearInterval(interval)
+    })
     this.on('startStats', function() {
         if (!isStarted) {
             isStarted = true
             player.currentStats = getCurrentStats()
-            setInterval(function() {
+            player.statsBuffered = [player.currentStats.buffered]
+            interval = setInterval(function() {
                 player.currentStats = getCurrentStats()
+                player.statsBuffered.push(player.currentStats.buffered)
+                if (player.statsBuffered.length > 60*4)
+                    player.statsBuffered.splice(0, 1)
                 var element = document.getElementById("infoPanel");
                 if (element)
                     element.parentNode.removeChild(element);
@@ -34,12 +41,28 @@ function statisticsPlugin(options) {
                 infoPanel.id = 'infoPanel'
                 for(var metric in player.currentStats) {
                     var div = document.createElement("div")
-                    var textnode = document.createTextNode(metric+': '+player.currentStats[metric])
-                    div.appendChild(textnode)
+                    if (metric == 'buffered') {
+                        var textnode = document.createTextNode(metric+': ')
+                        div.appendChild(textnode)
+                        var canvas = document.createElement('canvas');
+                        canvas.height = 20
+                        new Graph(player.statsBuffered, canvas, {
+                            background: "rgba(0,0,0,0)",
+                            lineWidth: 2,
+                            lineColor: "#F00"
+                        });
+                        div.appendChild(canvas)
+                        var textnode = document.createTextNode(player.currentStats[metric])
+                        div.appendChild(textnode)
+                    } else {
+                        var textnode = document.createTextNode(metric+': '+player.currentStats[metric])
+                        div.appendChild(textnode)
+                    }
+                    
                     infoPanel.appendChild(div)
                 }
                 document.body.appendChild(infoPanel)
-            }, 500)
+            }, 250)
             
         }
         
